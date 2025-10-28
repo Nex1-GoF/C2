b16a38fcb105029c2b3a2a62169e8c0e9f527267
taehyun <xogus5070318@gmail.com>
2025-10-28 10:15:44 +0900

    GOF-73 지도 API 연결

--- C2/C2/Views/MapPanel.xaml.cs
+++ C2/C2/Views/MapPanel.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using C2.ViewModels;
+using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsPresentation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,14 +19,30 @@ using System.Windows.Shapes;
 
 namespace C2.Views
 {
-    /// <summary>
-    /// MapPanel.xaml에 대한 상호 작용 논리
-    /// </summary>
     public partial class MapPanel : UserControl
     {
+
         public MapPanel()
         {
             InitializeComponent();
+
+            //PART_Map = new GMapControl(); // GMapControl 객체 초기화
+
+            Loaded += (s, e) =>
+            {
+                OnLoaded(s, e);
+            };
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            GMaps.Instance.Mode = AccessMode.ServerOnly; //(ServerAndCache->코드 실행->CacheOnly로 바꾸기)
+            //PART_Map.CacheLocation = @"C:\MapCache"; // C:\MapCache에 지도데이터를 넣어야겠지?
+            PART_Map.MapProvider = OpenStreetMapProvider.Instance;
+            PART_Map.MinZoom = 2;
+            PART_Map.MaxZoom = 18;
+            PART_Map.Zoom = 6;
+            PART_Map.Position = new PointLatLng(37.5665, 126.9780);
         }
     }
 }
