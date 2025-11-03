419a47ad9ec1923042ddf92e4159018f782eae6b
taehyun <xogus5070318@gmail.com>
2025-11-03 15:39:43 +0900

    지도 연결 완료
    지도 연결 완료, 전체 여백 지움

--- C2/C2/Views/MapPanel.xaml.cs
+++ C2/C2/Views/MapPanel.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using C2.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,12 @@ namespace C2.Views
     /// </summary>
     public partial class MapPanel : UserControl
     {
+        private readonly MapViewModel _vm;
         public MapPanel()
         {
             InitializeComponent();
+            _vm = new MapViewModel(PART_Map);
+            DataContext = _vm;
         }
     }
 }
