b5300d068b29abab87fedb58378a07c9882b7587
taehyun <xogus5070318@gmail.com>
2025-11-04 15:42:31 +0900

    표적 포커스 기능 구현

--- C2/C2/Views/MapPanel.xaml.cs
+++ C2/C2/Views/MapPanel.xaml.cs
@@ -38,11 +38,11 @@ namespace C2.Views
         {
             // 클릭 위치 (픽셀 기준)
             var point = e.GetPosition(PART_Map);
-            string mapClickText = $"지도 클릭: {point.X},{point.Y}";
+            
             
             // 테스트코드 => 지울 예정 => 바로 모델 호출
             LogService logService = LogService.Instance;
-            logService.AddLog(MessageType.System, mapClickText);
+            
 
             //------ 포커스 모드 ---------//
             GMapMarker? clickedMarker = null;
@@ -57,23 +57,33 @@ namespace C2.Views
                 double dy = point.Y - pos.Y;
                 double dist = Math.Sqrt(dx * dx + dy * dy);
 
-                if (dist < 20 && dist < minDist)
+                if (dist < 40 && dist < minDist)
                 {
                     clickedMarker = marker;
                     minDist = dist;
                 }
             }
 
-            if (clickedMarker == null) return;
+            if (clickedMarker == null)
+            {
+                string mapClickText = $"지도 클릭: {point.X},{point.Y}";
+                logService.AddLog(MessageType.System, mapClickText);
+                return;
+            }
 
             // 2️ 클릭된 객체의 ViewModel 추출
 
             if (clickedMarker.Shape is FrameworkElement frameworkElement && frameworkElement.DataContext is MissileMarkerViewModel missileVm)
             {
                 _vm.FocusMissileMarker(missileVm);
+                string mapClickText = "미사일 포커스 변경";
+                logService.AddLog(MessageType.System, mapClickText);
+
             }
             else if (clickedMarker.Shape is FrameworkElement frameworkElementTarget && frameworkElementTarget.DataContext is TargetMarkerViewModel targetVm)
             {
+                string mapClickText = "타겟 포커스 변경";
+                logService.AddLog(MessageType.System, mapClickText);
                 _vm.FocusTargetMarker(targetVm);
             }
 
