16aa88ed98fb60afe5fbd9be6b8e3f5e51e4091b
이주연 <jy122032@gmail.com>
2025-11-10 11:24:08 +0900

    refactor: 순환참조 제거

--- C2/C2/Network/TargetReceiver.cs
+++ C2/C2/Network/TargetReceiver.cs
@@ -1,68 +1,68 @@
 ﻿using C2.Models;
+using C2.Network;
 using C2.Services;
-using System;
 
-namespace C2.Network
+public class TargetReceiver
 {
-    public class TargetReceiver
+    private readonly TargetService _service;
+    private readonly SocketManager _socketManager;
+
+    public TargetReceiver(SocketManager socketManager)
     {
-        private readonly TargetService _service;
-        private readonly SocketManager _socketManager;
+        _service = TargetService.Instance;
+        _socketManager = socketManager;
 
-        public TargetReceiver(SocketManager socketManager)
+        // 이벤트 구독
+        _socketManager.TargetReceived += HandlePacket;
+    }
+
+    private void HandlePacket(TgtInfoPacket tgtInfo)
+    {
+        var target = ToTarget(tgtInfo);
+        _service.ReceiveTargetData(target);
+        SendToRadar(tgtInfo);
+    }
+
+    private void SendToRadar(TgtInfoPacket tgtInfo)
+    {
+        try
         {
-            _service = TargetService.Instance;
-            _socketManager = socketManager;
+            _socketManager.Send(tgtInfo.Serialize(), "127.0.0.1", 8003);
         }
-
-        public void HandlePacket(TgtInfoPacket tgtInfo)
+        catch (Exception ex)
         {
-            var target = ToTarget(tgtInfo);
-            _service.ReceiveTargetData(target);
-            SendToRadar(tgtInfo);
+            Console.WriteLine($"[RadarSend] Error: {ex.Message}");
         }
+    }
 
-        private void SendToRadar(TgtInfoPacket tgtInfo)
+    public static Target ToTarget(TgtInfoPacket packet)
+    {
+        int speed = (int)Math.Sqrt(packet.Vx * packet.Vx +
+                                   packet.Vy * packet.Vy +
+                                   packet.Vz * packet.Vz);
+
+        int altitude = packet.Z;
+        int yaw = (int)(Math.Atan2(packet.Y, packet.X) * 180.0 / Math.PI);
+        var curLoc = (Lat: packet.X / 1e7, Lon: packet.Y / 1e7);
+        var detectTime = DateTimeOffset.FromUnixTimeMilliseconds(packet.Timestamp).DateTime;
+
+        var target = new Target(
+            id: packet.Id[0],
+            speed: speed,
+            altitude: altitude,
+            yaw: yaw,
+            endLoc: curLoc,
+            detectTime: detectTime,
+            curLoc: curLoc
+        );
+
+        target.State = packet.Type switch
         {
-            try
-            {
-                _socketManager.Send(tgtInfo.Serialize(), "127.0.0.1", 8003);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"[RadarSend] Error: {ex.Message}");
-            }
-        }
+            'G' => TargetState.Guidance,
+            'T' => TargetState.Terminate,
+            _ => TargetState.Unknown
+        };
 
-        public static Target ToTarget(TgtInfoPacket packet)
-        {
-            int speed = (int)Math.Sqrt(packet.Vx * packet.Vx +
-                                       packet.Vy * packet.Vy +
-                                       packet.Vz * packet.Vz);
-
-            int altitude = packet.Z;
-            int yaw = (int)(Math.Atan2(packet.Y, packet.X) * 180.0 / Math.PI);
-            var curLoc = (Lat: packet.X / 1e7, Lon: packet.Y / 1e7);
-            var detectTime = DateTimeOffset.FromUnixTimeMilliseconds(packet.Timestamp).DateTime;
-
-            var target = new Target(
-                id: packet.Id[0],
-                speed: speed,
-                altitude: altitude,
-                yaw: yaw,
-                endLoc: curLoc,
-                detectTime: detectTime,
-                curLoc: curLoc
-            );
-
-            target.State = packet.Type switch
-            {
-                'G' => TargetState.Guidance,
-                'T' => TargetState.Terminate,
-                _ => TargetState.Unknown
-            };
-
-            return target;
-        }
+        return target;
     }
 }
