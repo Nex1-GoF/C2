6d475ac032a889100b43fce87e50a4e07ce61eb2
taehyun <xogus5070318@gmail.com>
2025-11-21 03:59:15 +0900

    feat: UI 글꼴 추가 및 적용

---
+++ C2/C2/Views/ManualWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.IO;
+using System.Windows;
+namespace C2.Views
+{
+    /// <summary>
+    /// ManualWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class ManualWindow : Window
+    {
+        public ManualWindow()
+        {
+            InitializeComponent();
+
+            // 프로젝트 내부 PDF 경로
+            string pdfPath = @"C:\workspace\C2\C2\C2\Resources\manual.pdf";
+
+
+            if (File.Exists(pdfPath))
+            {
+                PdfViewer.Source = new Uri(pdfPath);
+            }
+            else
+            {
+                MessageBox.Show("PDF 파일을 찾을 수 없습니다: " + pdfPath);
+            }
+        }
+    }
+}
